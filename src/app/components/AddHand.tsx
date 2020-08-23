a596aa3608fe36feafae3fd0da9d6eab358d34a5
Kevin Jiang <kevin51jiang@email.com>
2020-08-23 11:48:07 -0400

    working-ish

--- src/app/components/AddHand.tsx
+++ src/app/components/AddHand.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import '../styles/ui.css';
-import {Select} from 'antd';
-import {addHand} from './utils';
+import {Select, Input} from 'antd';
+// import {addHand} from './utils';
+import {SetStateAction, Dispatch} from 'react';
 
 const {Option} = Select;
 
 export interface AddHandProps {
     nextStep: () => void;
     prevStep: () => void;
+    sessionEntries: any[];
+    setSessionEntries: Dispatch<SetStateAction<any[]>>;
 }
 
 // all strings are values,
@@ -33,76 +36,92 @@ const AddHand: React.SFC<AddHandProps> = (Props: AddHandProps) => {
     const [clothes, setClothes] = React.useState<string>(clothesOptions[0]);
     const [pose, setPose] = React.useState<string>(poseOptions[0]);
     const [path, setPath] = React.useState<string>('');
+    const [name, setName] = React.useState(null);
 
     React.useEffect(() => {
         setPath(`https://emily.louie.ca/handz/${color}-in-${clothes}${poseOptions.indexOf(pose) + 1}.png`);
     }, [color, clothes, pose]);
 
     function sendData() {
-        addHand(color, clothes, poseOptions.indexOf(pose) + 1, true);
-        Props.nextStep();
+        Props.setSessionEntries([
+            ...Props.sessionEntries,
+            {color: color, clothes: clothes, pose: poseOptions.indexOf(pose) + 1, name: name ? name : 'You'},
+        ]);
+        // addHand(color, clothes, poseOptions.indexOf(pose) + 1, true);
+        Props.prevStep();
     }
 
     return (
         <>
-            {/* Color */}
-            <Select value={color} style={{width: '10rem'}} onChange={val => setColor(val)} optionLabelProp="label">
-                {colorOptions.map(colorOpt =>
-                    typeof colorOpt === 'string' ? (
-                        <Option value={colorOpt} label={colorOpt} key={`color-${colorOpt}`}>
+            <h2>Create custom hand</h2>
+            <div style={{margin: 'auto 1rem'}}>
+                <Input value={name} onChange={e => setName(e.target.value)} placeholder="Your name" />
+            </div>
+            <div>
+                {/* Color */}
+                <Select value={color} style={{width: '10rem'}} onChange={val => setColor(val)} optionLabelProp="label">
+                    {colorOptions.map(colorOpt =>
+                        typeof colorOpt === 'string' ? (
+                            <Option value={colorOpt} label={colorOpt} key={`color-${colorOpt}`}>
+                                <div style={{textAlign: 'left'}}>
+                                    <span style={{width: '1rem', display: 'inline-block'}} role="img">
+                                        {color === colorOpt ? '✓ ' : ' '}
+                                    </span>
+                                    {colorOpt}
+                                </div>
+                            </Option>
+                        ) : (
+                            <Option value={colorOpt[1]} label={colorOpt[0]} key={`color-${colorOpt[1]}`}>
+                                <div style={{textAlign: 'left'}}>
+                                    <span style={{width: '1rem', display: 'inline-block'}} role="img">
+                                        {color === colorOpt[1] ? '✓ ' : ' '}
+                                    </span>
+                                    {colorOpt[0]}
+                                </div>
+                            </Option>
+                        )
+                    )}
+                </Select>
+
+                {/* Clothes */}
+                <Select
+                    value={clothes}
+                    style={{width: '120px'}}
+                    onChange={val => setClothes(val)}
+                    optionLabelProp="label"
+                >
+                    {clothesOptions.map(clothOpt => (
+                        <Option value={clothOpt} label={clothOpt} key={`cloth-${clothOpt}`}>
                             <div style={{textAlign: 'left'}}>
                                 <span style={{width: '1rem', display: 'inline-block'}} role="img">
-                                    {color === colorOpt ? '✓ ' : ' '}
+                                    {clothes === clothOpt ? '✓ ' : ' '}
                                 </span>
-                                {colorOpt}
+                                {clothOpt}
                             </div>
                         </Option>
-                    ) : (
-                        <Option value={colorOpt[1]} label={colorOpt[0]} key={`color-${colorOpt[1]}`}>
+                    ))}
+                </Select>
+
+                {/* Pose */}
+                <Select
+                    value={pose}
+                    style={{width: '150px'}}
+                    onChange={val => setPose(val)}
+                    optionLabelProp="label"
+                    defaultValue={poseOptions[0]}
+                >
+                    {poseOptions.map((poseOpt, index) => (
+                        <Option value={poseOpt} label={poseOpt} key={`pose-${index}`}>
                             <div style={{textAlign: 'left'}}>
                                 <span style={{width: '1rem', display: 'inline-block'}} role="img">
-                                    {color === colorOpt[1] ? '✓ ' : ' '}
+                                    {pose === poseOpt ? '✓ ' : ' '}
                                 </span>
-                                {colorOpt[0]}
+                                {poseOptions[index]}
                             </div>
                         </Option>
-                    )
-                )}
-            </Select>
-
-            {/* Clothes */}
-            <Select value={clothes} style={{width: '120px'}} onChange={val => setClothes(val)} optionLabelProp="label">
-                {clothesOptions.map(clothOpt => (
-                    <Option value={clothOpt} label={clothOpt} key={`cloth-${clothOpt}`}>
-                        <div style={{textAlign: 'left'}}>
-                            <span style={{width: '1rem', display: 'inline-block'}} role="img">
-                                {clothes === clothOpt ? '✓ ' : ' '}
-                            </span>
-                            {clothOpt}
-                        </div>
-                    </Option>
-                ))}
-            </Select>
-
-            {/* Pose */}
-            <Select
-                value={pose}
-                style={{width: '150px'}}
-                onChange={val => setPose(val)}
-                optionLabelProp="label"
-                defaultValue={poseOptions[0]}
-            >
-                {poseOptions.map((poseOpt, index) => (
-                    <Option value={poseOpt} label={poseOpt} key={`pose-${index}`}>
-                        <div style={{textAlign: 'left'}}>
-                            <span style={{width: '1rem', display: 'inline-block'}} role="img">
-                                {pose === poseOpt ? '✓ ' : ' '}
-                            </span>
-                            {poseOptions[index]}
-                        </div>
-                    </Option>
-                ))}
-            </Select>
+                    ))}
+                </Select>
+            </div>
             <div>
                 {path === '' ? <img src={'https://emily.louie.ca/handz/Black-in-Basic1.png'} /> : <img src={path} />}
             </div>
--- src/app/components/utils.ts
+++ src/app/components/utils.ts
@@ -10,3 +10,13 @@ export const addHand = (color, clothes, pose, isUser = false) => {
         })
         .catch(err => console.error('Error occurred:', err));
 };
+
+export const createUUID = () => {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+    });
+    return uuid;
+};
