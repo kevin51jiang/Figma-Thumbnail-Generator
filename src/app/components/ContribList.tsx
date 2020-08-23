47861212fabfb0087d76d49a296963cce872b1a5
Kevin Jiang <kevin51jiang@email.com>
2020-08-23 00:57:44 -0400

    add posting

--- src/app/components/ContribList.tsx
+++ src/app/components/ContribList.tsx
@@ -10,26 +10,43 @@ export interface ContribListProps {
 }
 
 const ContribList: React.SFC<ContribListProps> = (Props: ContribListProps) => {
+    const [selected, setSelected] = React.useState([]);
+
+    const onSubmit = () => {
+        /**
+         * Posts array like this
+         * 
+            0: {name: "Emily Louie", color: "Brown", clothes: "Jumper", pose: "3"}
+            1: {name: "Leon Han", color: "Blue", clothes: "Jacket", pose: "2"}
+            2: {name: "Jayden Hsiao", color: "Dark-white", clothes: "Basic", pose: "4"}
+            3: {name: "Kevin Jiang", color: "Green", clothes: "Jacket", pose: "5"}
+         */
+        parent.postMessage({pluginMessage: {type: 'setCollaborators', collaborators: selected}}, '*');
+        Props.skip2Steps();
+    };
+
     return (
         <div className="contrib-list">
             <h2>Choose your collaborators</h2>
-
+            {console.log(selected)}
             <div className="team-select">
-                <ul>
-                    {Object.keys(people).map(person => (
-                        <>
-                            <li>
-                                <Checkbox>{people[person].name}</Checkbox>
-                            </li>
-                        </>
-                    ))}
-                </ul>
+                <Checkbox.Group onChange={newVal => setSelected(newVal)}>
+                    <ul>
+                        {Object.keys(people).map(person => (
+                            <>
+                                <li>
+                                    <Checkbox value={people[person]}>{people[person].name}</Checkbox>
+                                </li>
+                            </>
+                        ))}
+                    </ul>
+                </Checkbox.Group>
             </div>
             <div style={{float: 'left'}}>
                 <a onClick={Props.nextStep}>+ New</a>
             </div>
 
-            <button onClick={Props.skip2Steps} style={{position: 'absolute', bottom: '2rem', right: '2rem'}}>
+            <button onClick={onSubmit} style={{position: 'absolute', bottom: '2rem', right: '2rem'}}>
                 Next
             </button>
         </div>
